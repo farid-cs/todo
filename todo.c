d52b465833930a7018558249f0f0d852a0095693
Farid <faridfaracli9@gmail.com>
2024-11-09 02:06:47 +0400

    new coding style, seperate arg_to_id function

--- todo.c
+++ todo.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define TITLE_LENGTH 100
+#define LIST_LENGTH 100
+#define FILE_NAME "todo.bin"
 
 typedef struct Task Task;
 struct Task {
@@ -16,92 +18,92 @@ struct Todo {
 	size_t len;
 };
 
-const char* FILE_NAME = "todo.bin";
-
-Todo read_file(const char* file_name)
+Todo
+read_file(const char *filepath)
 {
 	Todo todo = {0};
-	FILE* file = fopen(file_name, "rb");
+	FILE *file = fopen(filepath, "rb");
 	if (file) {
-		fread(&todo, 1, sizeof(todo), file);
+		fread(&todo, 1, sizeof todo, file);
 		fclose(file);
 	}
 	return todo;
 }
 
-void save_todo_to_file(const char* file_name, Todo todo)
+void
+save_todo_to_file(const char *filepath, Todo todo)
 {
-	FILE* file = fopen(file_name, "wb");
-	fwrite(&todo, 1, sizeof(todo), file);
+	FILE *file = fopen(filepath, "wb");
+	fwrite(&todo, 1, sizeof todo, file);
 	fclose(file);
 }
 
-void ls(void)
+void
+ls(void)
 {
 	Todo todo = read_file(FILE_NAME);
 
 	for (size_t i = 0; i != todo.len; i++) {
-		const char* strike = "\x1B[9m";
-		const char* reset = "\x1B[0m";
+		const char *strike = todo.list[i].done ? "\x1B[9m" : "";
+		const char *reset = "\x1B[0m";
 
-		if (todo.list[i].done) {
-			printf("%lu %s%s%s\n", i+1, strike, todo.list[i].title, reset);
-		} else {
-			printf("%lu %s\n", i+1, todo.list[i].title);
-		}
+		printf("%lu %s%s%s\n", i+1, strike, todo.list[i].title, reset);
 	}
 }
 
-void add(const char* task)
+void
+add(const char *title)
 {
 	Todo todo = read_file(FILE_NAME);
+	char *dest = todo.list[todo.len].title;
 
-	if (sizeof(todo.list)/sizeof(*todo.list) <= todo.len) {
-		return;
+	if (LIST_LENGTH <= todo.len) {
+		fprintf(stderr, "error: too many tasks");
+		exit(1);
 	}
-
-	strcpy(todo.list[todo.len].title, task);
-	todo.list[todo.len].done = 0;
-	todo.len++;
-
+	strcpy(dest, title);
+	todo.list[todo.len++].done = 0;
 	save_todo_to_file(FILE_NAME, todo);
 }
 
-void rm(char* str_id)
+size_t
+arg_to_id(const char *arg)
+{
+	return strtoul(arg, NULL, 10) - 1;
+}
+
+void
+rm(char *str_id)
 {
 	Todo todo = read_file(FILE_NAME);
-	size_t id = strtol(str_id, NULL, 10) - 1;
+	size_t id = arg_to_id(str_id);
 
 	if (id >= todo.len) {
-		fprintf(stderr, "error: unknown task id -- '%s'\n", str_id);
+		fprintf(stderr, "error: task id out of range -- '%s'\n", str_id);
 		exit(1);
 	}
-
-	for (size_t i = id; i != todo.len; i++) {
-		memcpy(todo.list + i, todo.list + i + 1, sizeof(Task));
-	}
-
+	for (size_t i = id; i != todo.len; i++)
+		memcpy(todo.list + i, todo.list + i + 1, sizeof *todo.list);
 	todo.len--;
-
 	save_todo_to_file(FILE_NAME, todo);
 }
 
-void complete(char* str_id)
+void
+complete(char *str_id)
 {
 	Todo todo = read_file(FILE_NAME);
-	size_t id = strtol(str_id, NULL, 10) - 1;
+	size_t id = arg_to_id(str_id);
 
 	if (id >= todo.len) {
-		fprintf(stderr, "error: unknown task id -- '%s'\n", str_id);
+		fprintf(stderr, "error: task id out of range -- '%s'\n", str_id);
 		exit(1);
 	}
-
 	todo.list[id].done = 1;
-
 	save_todo_to_file(FILE_NAME, todo);
 }
 
-void usage()
+void
+usage()
 {
 	fputs("usage:\n"
 	"      todo [ls]\n"
@@ -111,20 +113,16 @@ void usage()
 	stderr);
 }
 
-int main(int argc, char** argv)
+int
+main(int argc, char *argv[])
 {
-	if (argc == 1) {
-		ls();
-		return 0;
-	}
-	if (!strcmp(argv[1], "ls")) {
+	if (argc == 1 || !strcmp(argv[1], "ls")) {
 		ls();
 		return 0;
 	}
 	if (!strcmp(argv[1], "add")) {
 		if (argc == 2) {
 			fputs("error: add requires a task\n", stderr);
-			usage();
 			return 1;
 		}
 		add(argv[2]);
@@ -133,7 +131,6 @@ int main(int argc, char** argv)
 	if (!strcmp(argv[1], "rm")) {
 		if (argc == 2) {
 			fputs("error: rm requires a task id\n", stderr);
-			usage();
 			return 1;
 		}
 		rm(argv[2]);
@@ -142,13 +139,12 @@ int main(int argc, char** argv)
 	if (!strcmp(argv[1], "do")) {
 		if (argc == 2) {
 			fputs("error: do requires a task id\n", stderr);
-			usage();
 			return 1;
 		}
 		complete(argv[2]);
 		return 0;
 	}
-	fprintf(stderr, "error: unknown command -- '%s'\n", argv[1]);
+	fprintf(stderr, "error: invalid command -- '%s'\n", argv[1]);
 	usage();
 	return 1;
 }
